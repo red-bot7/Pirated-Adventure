529a3ff4ae5966e6af94d17a9607bd5e8687c543
red-bot7 <cwork6129@gmail.com>
2025-11-27 17:12:17 -0600

    New Window

---
+++ PiratedAdventure/src/Main/GameWindow.java
@@ -0,0 +1,13 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends javax.swing.JFrame {
+    
+    public GameWindow() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setVisible(true);
+    }
+
+}
---
+++ PiratedAdventure/src/Main/mainClass.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class mainClass {
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+
+}
