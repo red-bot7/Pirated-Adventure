582819c50fe7fe9be8b283dd8f319e47724e6c3a
Clocky <cwork6129@gmail.com>
2026-06-23 11:53:45 -0500

    Fixed smol error

--- PiratedAdventure/src/Main/GamePanel.java
+++ PiratedAdventure/src/Main/GamePanel.java
@@ -78,7 +78,7 @@ public class GamePanel extends JPanel implements Runnable {
     public void paint(java.awt.Graphics g) {
         super.paint(g);
 		
-		g.drawImage(files.Background, 0, 0, Variables.WINDOW_WIDTH, Variables.WINDOW_HEIGHT, null);
+		g.drawImage(GameFiles.Background, 0, 0, Variables.WINDOW_WIDTH, Variables.WINDOW_HEIGHT, null);
 
 		tileManager.loadMap(g, currentMapIndex);
 		player.draw(g);
